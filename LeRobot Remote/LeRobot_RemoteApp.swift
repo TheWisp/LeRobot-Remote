e1f1080e72b7a8978bea1e6bf54318b577c44cfb
Fei Teng <feiteng@gmail.com>
2025-05-26 21:30:07 +0100

    Initial Commit

---
+++ LeRobot Remote/LeRobot_RemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeRobot_RemoteApp.swift
+//  LeRobot Remote
+//
+//  Created by Fei Teng on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct LeRobot_RemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
