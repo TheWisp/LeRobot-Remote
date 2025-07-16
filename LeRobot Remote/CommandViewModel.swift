0c2374d5cdb429ddd4ffcc6cfaeba576dc601448
Fei Teng <feiteng@gmail.com>
2025-07-16 11:13:08 +0100

    Clean up code to use mixture of CMMotion and ARKit

--- LeRobot Remote/CommandViewModel.swift
+++ LeRobot Remote/CommandViewModel.swift
@@ -34,7 +34,6 @@ class CommandViewModel: ObservableObject {
     // Constants
     static let motionAttitudeTolerance: Double = 0.05
     static let motionYawScale: Double = 3.2
-    //static let motionYawScale: Double = 1
     static let wristRollMin: Double = -55.0 //-90.0
     static let wristRollMax: Double = 75.0 //90.0
     static let wristFlexMin: Double = -90.0
@@ -57,28 +56,13 @@ class CommandViewModel: ObservableObject {
     var roll: Double = 0 {
         didSet { hasChanged = true }
     }
-    var shoulderPan: Double = 0
-    //var elbowFlex: Double = 0
-    
+
     var velocity: vector = .init(x: 0, y: 0, z: 0)
     var displacement: vector = .init(x: 0, y: 0, z: 0) {
         didSet { hasChanged = true }
     }
     
     var smoothedArKitZ: Double = 0.0
-    
-    func sign<T: Comparable & SignedNumeric>(_ x: T, _ epsilon: T) -> T {
-        return x > abs(epsilon) ? +1 : (x < abs(epsilon) * -1 ? -1 : 0)
-    }
-    
-    func signed(_ v: vector, _ epsilon: Double) -> vector {
-        return .init(
-            x: sign(v.x, epsilon),
-            y: sign(v.y, epsilon),
-            z: sign(v.z, epsilon)
-        )
-    }
-    
     
     init() {
 
@@ -93,57 +77,7 @@ class CommandViewModel: ObservableObject {
             return
         }
         
-        motionManager.startDeviceMotionUpdates(
-            using: frame,
-            to: .main
-        ) { [weak self] motion, error in
-            guard
-                let self   = self,
-                let motion = motion,
-                error       == nil
-            else { return }
-            
-            let t = motion.timestamp
-            if let t0 = self.lastTimestamp {
-                let dt = t - t0
-                
-                let a = motion.userAcceleration
-                // Rotate body acceleration → world frame
-                let r = motion.attitude.rotationMatrix
-                let accWorld = vector(
-                    x: r.m11 * a.x + r.m12 * a.y + r.m13 * a.z,
-                    y: r.m21 * a.x + r.m22 * a.y + r.m23 * a.z,
-                    z: r.m31 * a.x + r.m32 * a.y + r.m33 * a.z
-                )
-                
-                //print("Acceleration (m/s²): \(accWorld)")
-                
-                // Drift correction example
-                if accWorld.lengthSquared() < 0.001 {
-                    // assume stationary → zero out tiny velocities
-                    self.velocity = .init(x: 0, y: 0, z: 0)
-                } else {
-                    // Simple Euler integration
-                    self.velocity    += accWorld * dt
-                }
-
-                //print("X velocity: \(String(format: "%.3f", self.velocity.x))")
-                //print("Y velocity: \(String(format: "%.3f", self.velocity.y))")
-                //print("Z velocity: \(String(format: "%.3f", self.velocity.z))")
-                //print("")
-                
-                shoulderPan = self.velocity.y
-                //elbowFlex = self.velocity.z
-                
-                //self.displacement += self.velocity * dt
-                
-                // displacement is now in metres, from the moment you called init()
-                //print("Displacement (m): \(self.displacement)")
-            }
-            
-            // always update the timestamp _after_ you’ve used it
-            self.lastTimestamp = t
-        }
+        motionManager.startDeviceMotionUpdates(using: frame)
     }
     
     func handleJoystickMove(x: Double, y: Double) {
@@ -152,8 +86,6 @@ class CommandViewModel: ObservableObject {
     }
     
     func handleMotion() {
-        
-        
         guard let attitude = motionManager.deviceMotion?.attitude else { return }
         
         let yawRadian = attitude.yaw //TODO currently this gets incorrect value when the screen is facing down
@@ -167,7 +99,6 @@ class CommandViewModel: ObservableObject {
         if abs(self.roll - rollDegree) > CommandViewModel.motionAttitudeTolerance {
             self.roll = rollDegree
         }
-        
     }
     
     func update() {
@@ -186,9 +117,7 @@ class CommandViewModel: ObservableObject {
             xCmd: left * CommandViewModel.moveXYSpeedHigh,
             yCmd: forward * CommandViewModel.moveXYSpeedHigh,
             thetaCmd: 0)
-        
-        // TODO partial update each motor
-        
+
         // Arm
         // The motor states are in [-90, 90] after calibration,
         // whereas the yaw is 90 when rotated to the left, -90 to the right
@@ -198,19 +127,7 @@ class CommandViewModel: ObservableObject {
         
         // The phone's roll is 0 when laying flat. This should map to gripper looking down.
         let wristFlex = max(min(-roll + 90, CommandViewModel.wristFlexMax), CommandViewModel.wristFlexMin)
-        
-        /* rest position ~=
-         shoulder_pan: 7.294921875
-         > shoulder_lift: 191.07421875
-         > elbow_flex: 180.615234375
-         wrist_flex: 61.875
-         wrist_roll: -3.8671875
-         gripper: -2.2696011066436768
-         */
-        
-        let phoneVeloToPanScale = 720.0
-        let panSpeed = degpsToRaw(shoulderPan * phoneVeloToPanScale)
-        
+
         //experimental, when lifted 0.01m = changing from 180 to 135 degrees
         let shoulderLiftStart = 191.0
         let shoulderLiftEnd = 135.0
